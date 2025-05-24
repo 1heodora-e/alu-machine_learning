aee91141250527f59310c805ea2054e14681b52c
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 23:50:44 +0200

    This is a file

--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -3,4 +3,4 @@
 
 def np_transpose(matrix):
     """Returns the transpose of a 2D matrix."""
-    return [list(row) for row in zip(*matrix)]
+    return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
