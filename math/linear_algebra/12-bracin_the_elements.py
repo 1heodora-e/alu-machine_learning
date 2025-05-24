e872e093bc34ec29e63f4765b197fe584f515fc2
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 23:42:26 +0200

    This is a Linear Algebra File

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
+"""Performs element-wise addition, subtraction, multiplication, and division of two arrays."""
+
 
 def np_elementwise(mat1, mat2):
+    """Returns a tuple of element-wise operations: (add, sub, mul, div)."""
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
