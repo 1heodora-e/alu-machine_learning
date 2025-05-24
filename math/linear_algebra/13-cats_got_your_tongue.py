d86ae00a936b4cbb5034b51015f854df0ea92bdb
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 23:41:02 +0200

    This is a Linear Alegbra File

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Function that concatenates two numpy arrays along a specified axis."""
+
 import numpy as np
 
+
 def np_cat(mat1, mat2, axis=0):
+    """Concatenates two numpy arrays along a given axis."""
     return np.concatenate((mat1, mat2), axis=axis)
