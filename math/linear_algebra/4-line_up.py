0773e15a05fec36838083ccacd9e4710b19f0998
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 23:46:57 +0200

    This is a Linear Algebra File

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+"""This module defines a function that adds two arrays element-wise.
+"""
+
 def add_arrays(arr1, arr2):
-    """
-    Adds two arrays element-wise
-    """
+    """Adds two arrays element-wise and returns the result."""
     if len(arr1) != len(arr2):
         return None
     return [a + b for a, b in zip(arr1, arr2)]
--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""This module defines a function that adds two 2D matrices element-wise.
+"""
+
 def add_matrices2D(mat1, mat2):
-    """
-    Adds two 2D matrices element-wise
-    """
-    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
+    """Adds two 2D matrices element-wise and returns the result."""
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
-    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return [
+        [mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
+        for i in range(len(mat1))
+    ]
