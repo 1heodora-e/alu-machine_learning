faafe3284115544bee54774398e9571a4e79b74a
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 19:06:12 +0200

    This is matirx code

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 """This module provides a function that calculates the shape of a matrix."""
 
+
 def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+
+    Args:
+        matrix (list): A nested list representing the matrix.
+
+    Returns:
+        list: A list of integers representing the shape.
+    """
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
