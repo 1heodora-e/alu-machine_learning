9054a6302c74489ab3a6ec00c49d5238bfdd635d
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 19:04:46 +0200

    This is a matrix file

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+"""This module provides a function that calculates the shape of a matrix."""
+
 def matrix_shape(matrix):
     shape = []
     while isinstance(matrix, list):
