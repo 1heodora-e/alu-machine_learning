915386d053c9f3694477633155ac159a8c4e5e07
Theodora Egbunike <t.egbunike@alustudent.com>
2025-05-24 23:39:03 +0200

    This is a Linear Alegbra File

--- math/linear_algebra/14-saddle_up.py
+++ math/linear_algebra/14-saddle_up.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""Function that performs matrix multiplication using numpy."""
+
 import numpy as np
 
+
 def np_matmul(mat1, mat2):
+    """Multiplies two matrices using numpy.matmul."""
     return np.matmul(mat1, mat2)
