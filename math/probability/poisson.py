30b7dc3d7fb619c0c79e8291c89ce052edc7210c
Theodora Egbunike <t.egbunike@alustudent.com>
2025-06-21 21:04:42 +0200

    This is the poisson.py file

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python3
-"""Creating a class that represents a poisson distribution."""
+"""
+Poisson distribution class
+"""
 
 
 class Poisson:
-    """Creating the Poisson class
+    """
+    Represents a Poisson distribution
     """
 
     def __init__(self, data=None, lambtha=1.):
-        """ Constructor
         """
-        self.E = 2.7182818285
-        self.PI = 3.1415926536
-
+        Initializes the Poisson distribution
+        """
         if data is None:
             if lambtha <= 0:
-                raise ValueError('lambtha must be a positive value')
-            else:
-                self.lambtha = float(lambtha)
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
-                raise TypeError('data must be a list')
+                raise TypeError("data must be a list")
             if len(data) < 2:
-                raise ValueError('data must contain multiple values')
+                raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
 
-    def factorial(self, k):
-        """Obtaining the factorial of a number."""
-        result = 1
-        for i in range(1, k+1):
-            result = result * i
-        return result
-
     def pmf(self, k):
-        """Calculates the value of the PMF (probability mass function)
-        for a given number of “successes”"""
-        if not isinstance(k, int):
-            k = int(k)
+        """
+        Calculates the value of the PMF
+        for a given number of occurrences (k)
+        """
         if k < 0:
             return 0
 
-        p = (self.lambtha ** k) * (self.E ** -self.lambtha) / self.factorial(k)
-        return p
+        k = int(k)  # Ensure k is an integer
+
+        # Helper function to calculate factorial
+        def factorial(n):
+            if n == 0 or n == 1:
+                return 1
+            result = 1
+            for i in range(2, n + 1):
+                result *= i
+            return result
+
+        # Apply the PMF formula for Poisson distribution
+        e = 2.7182818285  # Euler's number
+        lambtha = self.lambtha
+        return (lambtha ** k) * (e ** (-lambtha)) / factorial(k)
 
     def cdf(self, k):
-        """Calculates the value of the CDF (cumulative distribution function)
-        for a given number of “successes”"""
-        if not isinstance(k, int):
-            k = int(k)
+        """
+        Calculates the value of the CDF for a
+        given number of successes (k)
+        """
         if k < 0:
             return 0
 
-        def p(k):
+        k = int(k)  # Ensure k is an integer
 
-            p = (self.lambtha ** k) * (self.E ** -
-                                       self.lambtha) / self.factorial(k)
-            return p
-        return sum([p(k) for k in range(0, k+1)])
+        # Sum the PMFs from 0 to k
+        cumulative_prob = 0
+        for i in range(k + 1):
+            cumulative_prob += self.pmf(i)
+
+        return cumulative_prob
